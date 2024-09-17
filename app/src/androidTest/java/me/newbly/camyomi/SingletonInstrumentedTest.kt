6b983652ce3e96b76eb7cb2c0837bf8fe6c86f4f
Aaron M. Serrano <aaronserrano.nueca@gmail.com>
2024-09-17 10:27:03 +0800

    Add Singleton unit and instrumented test

---
+++ app/src/androidTest/java/me/newbly/camyomi/SingletonInstrumentedTest.kt
@@ -0,0 +1,33 @@
+package me.newbly.camyomi
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import me.newbly.camyomi.database.JMdictDatabase
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class SingletonInstrumentedTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Inject lateinit var database1: JMdictDatabase
+    @Inject lateinit var database2: JMdictDatabase
+
+    @Test
+    fun testSingletonInstance() {
+        assertThat(database1, `is`(database2))
+    }
+}
