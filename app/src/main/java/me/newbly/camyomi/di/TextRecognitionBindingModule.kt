3c5ae4e013151937cb4ae0086ccfdfdeff074381
Aaron M. Serrano <aaronserrano.nueca@gmail.com>
2024-09-23 16:13:12 +0800

    Change `TextRecognitionBindingModule` scope to ActivityComponent

--- app/src/main/java/me/newbly/camyomi/di/TextRecognitionBindingModule.kt
+++ app/src/main/java/me/newbly/camyomi/di/TextRecognitionBindingModule.kt
@@ -3,20 +3,17 @@ package me.newbly.camyomi.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.FragmentComponent
-import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.android.components.ActivityComponent
 import me.newbly.camyomi.data.local.mlkit.MLKitService
 import me.newbly.camyomi.data.repository.TextRecognitionRepository
 import me.newbly.camyomi.presentation.contract.TextRecognitionContract
 
 @Module
-@InstallIn(FragmentComponent::class)
+@InstallIn(ActivityComponent::class)
 abstract class TextRecognitionBindingModule {
     @Binds
-    @FragmentScoped
     abstract fun provideTextRecognitionRepository(repository: TextRecognitionRepository): TextRecognitionContract.Repository
 
     @Binds
-    @FragmentScoped
     abstract fun provideTextRecognitionService(service: MLKitService): TextRecognitionContract.DataSource
 }
