f7d08aa5a378edfe0f2d8bcc2fc4c59d11cd8ed3
Aaron M. Serrano <aaronserrano.nueca@gmail.com>
2024-10-01 10:59:31 +0800

    Change DI modules' scope to Singleton
    Most of these don't have to run on the Activity scope.

--- app/src/main/java/me/newbly/camyomi/di/RepositoryBindingModule.kt
+++ app/src/main/java/me/newbly/camyomi/di/RepositoryBindingModule.kt
@@ -3,18 +3,21 @@ package me.newbly.camyomi.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import me.newbly.camyomi.data.repository.AppDbRepository
 import me.newbly.camyomi.data.repository.JMdictRepository
 import me.newbly.camyomi.presentation.contract.AppDbContract
 import me.newbly.camyomi.presentation.contract.JMdictContract
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class RepositoryBindingModule {
     @Binds
+    @Singleton
     abstract fun provideJMdictRepository(repository: JMdictRepository): JMdictContract.Repository
 
     @Binds
+    @Singleton
     abstract fun provideAppDbRepository(repository: AppDbRepository): AppDbContract.Repository
 }
--- app/src/main/java/me/newbly/camyomi/di/TextRecognitionBindingModule.kt
+++ app/src/main/java/me/newbly/camyomi/di/TextRecognitionBindingModule.kt
@@ -3,17 +3,20 @@ package me.newbly.camyomi.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import me.newbly.camyomi.data.local.mlkit.MLKitService
 import me.newbly.camyomi.data.repository.TextRecognitionRepository
 import me.newbly.camyomi.presentation.contract.TextRecognitionContract
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class TextRecognitionBindingModule {
     @Binds
+    @Singleton
     abstract fun provideRepository(repository: TextRecognitionRepository): TextRecognitionContract.Repository
 
     @Binds
+    @Singleton
     abstract fun provideDataSource(service: MLKitService): TextRecognitionContract.DataSource
 }
