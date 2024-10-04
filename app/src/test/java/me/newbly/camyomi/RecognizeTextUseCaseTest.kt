0ee447e1164ad694f84184e8a4ea4ddf85dd6b53
Aaron M. Serrano <aaronserrano.nueca@gmail.com>
2024-10-04 20:11:04 +0800

    `RecognizeTextUseCaseText` now extends to `BaseTest`

--- app/src/test/java/me/newbly/camyomi/RecognizeTextUseCaseTest.kt
+++ app/src/test/java/me/newbly/camyomi/RecognizeTextUseCaseTest.kt
@@ -4,45 +4,37 @@ import android.graphics.Bitmap
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
 import me.newbly.camyomi.domain.usecase.RecognizeTextUseCase
-import me.newbly.camyomi.presentation.contract.TextRecognitionContract
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
-import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.any
-import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.withSettings
 
 @RunWith(MockitoJUnitRunner::class)
-class RecognizeTextUseCaseTest {
-
-    @Mock
-    lateinit var mockRepository: TextRecognitionContract.Repository
-    lateinit var recognizeTextUseCase: RecognizeTextUseCase
+class RecognizeTextUseCaseTest : BaseTest() {
+    private lateinit var recognizeTextUseCase: RecognizeTextUseCase
 
     @Before
-    fun setUp() {
-        MockitoAnnotations.openMocks(this)
-        recognizeTextUseCase = RecognizeTextUseCase(mockRepository)
+    override fun setUp() {
+        recognizeTextUseCase = RecognizeTextUseCase(mockTextRecognitionRepository)
     }
 
     @Test
-    fun `test recognize text from image`() = runBlocking {
-        `when`(
-            mockRepository.processImageForOCR(any())
-        )
-            .doReturn(
-                Result.success("こんにちは")
-            )
-
+    fun `recognize text from image`() = runBlocking {
         val dummyImage = mock<Bitmap>(withSettings(lenient = true))
+
+        `when`(
+            mockTextRecognitionRepository.processImageForOCR(dummyImage)
+        ).thenReturn(
+            Result.success("こんにちは")
+        )
+
         val result = recognizeTextUseCase(dummyImage)
 
+        verify(mockTextRecognitionRepository).processImageForOCR(dummyImage)
         assertEquals("こんにちは", result.getOrNull())
     }
-
 }
