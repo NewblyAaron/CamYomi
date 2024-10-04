7f3d0e6f9454b7ded2a02cd53197ee8dcfcec436
Aaron M. Serrano <aaronserrano.nueca@gmail.com>
2024-10-04 20:11:18 +0800

    More unit tests for `SingletonUnitTest`

--- app/src/test/java/me/newbly/camyomi/SingletonUnitTest.kt
+++ app/src/test/java/me/newbly/camyomi/SingletonUnitTest.kt
@@ -3,7 +3,10 @@ package me.newbly.camyomi
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
+import me.newbly.camyomi.data.local.app.AppDatabase
 import me.newbly.camyomi.data.local.jmdictdb.JMdictDatabase
+import me.newbly.camyomi.presentation.contract.AppDbContract
+import me.newbly.camyomi.presentation.contract.JMdictContract
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
@@ -22,15 +25,37 @@ class SingletonUnitTest {
     var hiltRule = HiltAndroidRule(this)
 
     @Before
-    fun setup() {
+    fun setUp() {
         hiltRule.inject()
     }
 
-    @Inject lateinit var database1: JMdictDatabase
-    @Inject lateinit var database2: JMdictDatabase
+    @Inject
+    lateinit var jmdictDatabase1: JMdictDatabase
+    @Inject
+    lateinit var jmdictDatabase2: JMdictDatabase
+    @Inject
+    lateinit var appDatabase1: AppDatabase
+    @Inject
+    lateinit var appDatabase2: AppDatabase
 
     @Test
-    fun testSingletonInstance() {
-        assertThat(database1, `is`(database2))
+    fun `databases are singleton`() {
+        assertThat(jmdictDatabase1, `is`(jmdictDatabase2))
+        assertThat(appDatabase1, `is`(appDatabase2))
+    }
+
+    @Inject
+    lateinit var appRepository1: AppDbContract.Repository
+    @Inject
+    lateinit var appRepository2: AppDbContract.Repository
+    @Inject
+    lateinit var jmdictRepository1: JMdictContract.Repository
+    @Inject
+    lateinit var jmdictRepository2: JMdictContract.Repository
+
+    @Test
+    fun `repositories are singleton`() {
+        assertThat(appRepository1, `is`(appRepository2))
+        assertThat(jmdictRepository1, `is`(jmdictRepository2))
     }
 }
