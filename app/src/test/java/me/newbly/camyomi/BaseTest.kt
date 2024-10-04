0dbac4eb8c5ada97b74c5f6e0e9c409289be632b
Aaron M. Serrano <aaronserrano.nueca@gmail.com>
2024-10-04 20:00:24 +0800

    Create `BaseTest` class for initializing mock repositories

---
+++ app/src/test/java/me/newbly/camyomi/BaseTest.kt
@@ -0,0 +1,19 @@
+package me.newbly.camyomi
+
+import me.newbly.camyomi.presentation.contract.AppDbContract
+import me.newbly.camyomi.presentation.contract.JMdictContract
+import me.newbly.camyomi.presentation.contract.TextRecognitionContract
+import org.junit.Before
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+open class BaseTest {
+    @Mock protected lateinit var mockTextRecognitionRepository: TextRecognitionContract.Repository
+    @Mock protected lateinit var mockAppRepository: AppDbContract.Repository
+    @Mock protected lateinit var mockJMdictRepository: JMdictContract.Repository
+
+    @Before
+    open fun setUp() {
+        MockitoAnnotations.openMocks(this)
+    }
+}
